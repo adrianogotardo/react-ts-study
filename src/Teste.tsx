309413bcd035004e1f6c6b4e7db37a9de02a89ce
Adriano Gotardo <adrianogotardo.12@gmail.com>
2023-05-13 12:41:59 -0300

    Reinitialization of the project due to previous mistakes

---
+++ src/Teste.tsx
@@ -0,0 +1,12 @@
+
+interface Props {
+    text?: string
+}
+
+export default function Teste ({}: Props): JSX.Element {
+    return(
+        <div>
+            "hello world"
+        </div>
+    )
+}
